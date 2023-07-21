3592b58fadeec2ed77b795e1fba25a31c7e2fc05
italojohnny <italojohnnydosanjos@gmail.com>
2023-07-21 19:08:05 -0300

    Cria teste que verifica nomes das moedas

--- getter_rates/test.py
+++ getter_rates/test.py
@@ -8,3 +8,10 @@ class GetterRatesTestCase(TestCase):
         result = get_by_date()
         return_type = isinstance(result, dict)
         self.assertTrue(return_type, 'Tipo do retorno invalido')
+
+    def test_coins_names(self):
+        result = get_by_date()
+        coins_names = [i for i in result.keys() if i != 'date']
+        self.assertIn('brl', coins_names, 'moeda brl nao encontrada')
+        self.assertIn('eur', coins_names, 'modea eur nao encontrada')
+        self.assertIn('jpy', coins_names, 'moeda jpy nao encontrada')
