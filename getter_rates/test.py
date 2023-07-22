da6fdc106d31f76701bada033cd2d9a8473c9863
italojohnny <italojohnnydosanjos@gmail.com>
2023-07-21 21:38:45 -0300

    Corrige estido de codigo

--- getter_rates/test.py
+++ getter_rates/test.py
@@ -5,13 +5,11 @@ from datetime import date, timedelta
 
 
 class GetterRatesTestCase(TestCase):
-
     def test_return_type(self):
         result = get_by_date()
         return_type = isinstance(result, dict)
 
         self.assertTrue(return_type, 'Tipo do retorno invalido')
-
 
     def test_coins_names(self):
         result = get_by_date()
@@ -20,26 +18,26 @@ class GetterRatesTestCase(TestCase):
         self.assertIn('eur', coins_names, 'modea eur nao encontrada')
         self.assertIn('jpy', coins_names, 'moeda jpy nao encontrada')
 
-
     def test_good_date(self):
         yesterday = date.today() - timedelta(days=1)
         result = get_by_date(yesterday)
         return_type = isinstance(result, dict)
         self.assertTrue(return_type, 'Tipo do retorno invalido')
 
-
     def test_future_date(self):
         today = date.today()
         tomorrow = today + timedelta(days=1)
         result = get_by_date(tomorrow)
-        self.assertEqual(today, result.get('date'), 'Data diferente do esperado')
-
+        self.assertEqual(
+            today, result.get('date'), 'Data diferente do esperado'
+        )
 
     def test_good_string_date(self):
         today = str(date.today())
         result = get_by_date(today)
-        self.assertEqual(today, str(result.get('date')), 'Data diferente do esperado')
-
+        self.assertEqual(
+            today, str(result.get('date')), 'Data diferente do esperado'
+        )
 
     def test_very_old_date(self):
         very_old_date = date(1500, 4, 22)
@@ -50,8 +48,9 @@ class GetterRatesTestCase(TestCase):
         except HTTPError as err:
             status_code = err.response.status_code
 
-        self.assertEqual(status_code, 500, 'codigo de retorno diferente do esperado')
-
+        self.assertEqual(
+            status_code, 500, 'codigo de retorno diferente do esperado'
+        )
 
     def test_bad_string_date(self):
         string = 'abcdefgh'
@@ -62,4 +61,6 @@ class GetterRatesTestCase(TestCase):
         except HTTPError as err:
             status_code = err.response.status_code
 
-        self.assertEqual(status_code, 400, 'codigo de retorno diferente do esperado')
+        self.assertEqual(
+            status_code, 400, 'codigo de retorno diferente do esperado'
+        )
