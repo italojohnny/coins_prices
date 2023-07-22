6e03ec8725fd67efdadbdc179c90a4bb52828681
italojohnny <italojohnnydosanjos@gmail.com>
2023-07-22 14:53:21 -0300

    Cria funcao para obter dados de apenas de uma moeda

--- main_app/utils.py
+++ main_app/utils.py
@@ -2,11 +2,19 @@ from getter_rates import get_by_date
 from datetime import date
 
 
-def get_rates():
+def get_rates(coin_name):
     return [
-        get_by_date(date(2023, 7, 17)),
-        get_by_date(date(2023, 7, 18)),
-        get_by_date(date(2023, 7, 19)),
-        get_by_date(date(2023, 7, 20)),
-        get_by_date(date(2023, 7, 21)),
+        get_rate(date(2023, 7, 17), coin_name),
+        get_rate(date(2023, 7, 18), coin_name),
+        get_rate(date(2023, 7, 19), coin_name),
+        get_rate(date(2023, 7, 20), coin_name),
+        get_rate(date(2023, 7, 21), coin_name),
     ]
+
+
+def get_rate(d, coin_name):
+    result = get_by_date(d)
+    return {
+        'date': result.get('date'),
+        'value': result.get(coin_name),
+    }
