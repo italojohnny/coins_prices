09a94fbc5a1d4395c2c9c230823d57d4211f2dab
italojohnny <italojohnnydosanjos@gmail.com>
2023-07-22 14:56:12 -0300

    Altera view index para informar nome de moeda

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,9 @@ from main_app import utils
 
 
 def index(request):
+    coin_name = 'jpy'
     context = {
-        'rates': utils.get_rates(),
+        'rates': utils.get_rates(coin_name),
+        'coin': coin_name,
     }
     return render(request, 'index.html', context)
