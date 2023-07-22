e2487af9753a9bd6e6dc21426acfe5d86e0dca62
italojohnny <italojohnnydosanjos@gmail.com>
2023-07-22 09:21:01 -0300

    Cria view basica do main_app

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    context = {
+        'message': 'hello world',
+    }
+    return render(request, 'index.html', context)
