0f4da458ec4d347e5782e8113c80b92d2de24dc1
italojohnny <italojohnnydosanjos@gmail.com>
2023-07-22 12:56:49 -0300

    Acrescenta novo teste para a model

--- main_app/tests.py
+++ main_app/tests.py
@@ -10,3 +10,20 @@ class RateModelTestCase(TestCase):
         inserted.save()
         consulted = RateModel.objects.filter(date=day).first()
         self.assertEqual(inserted, consulted)
+
+    def test_insert_date_already_saved(self):
+        day = date(2023, 1, 1)
+        rates1 = {'brl': 1.0, 'eur': 2.0, 'jpy': 3.0}
+        rates2 = {'brl': 4.0, 'eur': 5.0, 'jpy': 6.0}
+
+        first = RateModel(date=day, **rates1)
+        first.save()
+
+        latest, fail = RateModel.objects.update_or_create(
+            date=day, defaults=rates2
+        )
+
+        recovered = RateModel.objects.filter(date=day).first()
+
+        self.assertNotEqual(first.brl, recovered.brl)
+        self.assertEqual(latest.brl, recovered.brl)
