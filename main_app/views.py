6bf193d6281a2d5c6cce5d6b49775655590ed278
italojohnny <italojohnnydosanjos@gmail.com>
2023-07-23 12:37:02 -0300

    Define valores padroes para exibicao da view index

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from .forms import FormIndex
 from main_app import utils
+from datetime import date, timedelta
 
 
 def index(request):
     coin_name = 'jpy'
+    date2 = date.today()
+    date1 = date2 - timedelta(days=4)
+    rates = None
 
     form = FormIndex(initial={'coin': coin_name})
 
+    rates = utils.get_rates(coin_name, date1, date2)
+
     context = {
-        'rates': utils.get_rates(coin_name),
+        'rates': rates,
         'coin': coin_name,
         'form': form,
     }
